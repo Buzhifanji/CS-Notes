a0400cead098bb09b37e7f9f43d7a22a2a4431a9
huangbin <buzhifanji@163.com>
2021-08-12 10:05:34 +0800

    vue响应式原理笔记

---
+++ code/JavaScript/vue2/min-vue/js/observer.js
@@ -0,0 +1,32 @@
+class Observer {
+    constructor(data) {
+        this.walk(data)
+    }
+    walk(data) {
+        if(data && typeof data === 'object') {
+            Object.keys(data).forEach(key => {
+                this.defineReactive(data, key, data[key])
+            })
+        }
+    }
+    defineReactive(data, key, value) {
+        // 递归处理 嵌套对象
+        this.walk(value)
+        const _this = this
+        Object.defineProperty(data, key, {
+            configurable: true,
+            enumerable: true,
+            get() {
+                return value
+            },
+            set(newValue) {
+            if(newValue === value) {
+                return
+            }
+            value = newValue
+            // 处理修改的时候，是新增对象的情况
+            _this.walk(value)
+            }
+        })
+    }
+}
