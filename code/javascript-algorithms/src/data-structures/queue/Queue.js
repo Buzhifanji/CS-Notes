7819e9aee4e390306667bb8043005741e575f116
buzhifanji <buzhifanji@163.com>
2021-12-18 21:13:57 +0800

    基于链表 实现队列 数据结构

---
+++ code/javascript-algorithms/src/data-structures/queue/Queue.js
@@ -0,0 +1,44 @@
+import LinkedList from '../linked-list/LinkedList';
+
+export default class Queue {
+    constructor() {
+        // 通过链表来实现队列
+        this.linkedList = new LinkedList()
+    }
+    /**
+     * 判断队列是否为空
+     * @return {boolean}
+     */
+    isEmpty() {
+        return !this.linkedList.head
+    }
+    /**
+     * 读取队列中第一个元素.
+     * @return {*}
+     */
+    peek() {
+        if (this.isEmpty()) {
+            return null
+        }
+
+        return this.linkedList.head.value
+    }
+    /**
+     * 向队列添加一个元素（链表末尾添加一个元素）
+     * @param {*} value 
+     */
+    enqueue(value) {
+        this.linkedList.append(value)
+    }
+    /**
+     * 移除队列中的一个元素
+     * @returns {*}
+     */
+    dequeue() {
+        const removeHead = this.linkedList.deleteHead()
+        return removeHead ? removeHead.value : null
+    }
+    toString(callback) {
+        return this.linkedList.toString(callback)
+    }
+}
